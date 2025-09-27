5bd1eb99ef035a2e4ded0fb19cd69670f2cd795a
Марат <marat.tskh@gmail.com>
2025-09-27 14:06:24 +0300

    Alpex migration

--- init.sql
+++ init.sql
@@ -22,14 +22,16 @@ INSERT INTO public.gateway (gtw_adapter_id, gtw_params_jsonb)
 VALUES ('paylink', '{"transport": {"base_address": "https://payadap.co/api/v1/"}, "payment_methods": [{"id": "p2pcard", "name": "P2P Card", "required_fields": []}]}'),
  ('auris', '{"transport": {"base_address": "https://changer.icu/api2"}, "payment_methods": [{"id": "p2pcard", "gtw_id": {"KGS": 237, "KZT": 260, "RUB": 86}, "required_fields": []}, {"id": "p2psbp", "gtw_id": {"RUB": 88}, "required_fields": []}]}'),
  ('sequoia', '{"transport": {"base_address": "https://sequoiapay.com"}, "payment_methods": [{"id": "p2pcard", "name": "P2P Card", "required_fields": [{"id": "bank", "name": "Bank", "type": "select", "values": [{"id": "ru_sberbank", "name": "Sberbank"}]}]}]}'),
- ('asupayme', '{"transport": {"base_address":"http://185.230.143.210:8021"}}');
+ ('asupayme', '{"transport": {"base_address":"http://185.230.143.210:8021"}}'),
+ ('alpex', '{"transport": {"base_address": "http://5.129.242.10:8022"}}')
 
 
 INSERT INTO public.channel (chn_name, gtw_id, chn_params_jsonb)
 VALUES ('paylink', 1, '{"credentials": {"api_key": "m9bb1lelXNK3c198UWb2e41J1EQM", "merch_id": "52161-b615-4211-8638-3998bdb806c0"}, "payment_methods": ["p2pcard"]}'),
  ('auris', 2, '{"credentials": {"api_key": "049050051052-8ECG0WKvwSTWBzHTv2qtzy6MTiCkEoT2QAVAmNoGfQPzm02sPuBtz4nRwCJRs6fmM0WHUNqke6gG8o1MKcVHZGpJScZtOt8SmC3X1PGRx9uMTL3rw-OQP244B0M3HTID6T8PixLROqwaYQHJq6", "shop_id": 1102, "secret_key": "049050051052-jVDNvl8Lm8U4LQ7NPFGP2K03-k7w0o06k"}, "payment_methods": ["p2pcard"]}'),
  ('sequoia', 3, '{"credentials": {"secret_key": "8f5DSbXOolXwhLVmtOd", "callback_secret": "fqLFXw9BQLbW7G83Uf"}, "payment_methods": ["p2pcard"]}'),
- ('asupayme', 4, '{"credentials": {"api_key": "c2RmZHNhYnZmZGFiYWV0dnJ0c3JhZGZoYnN0cmRmdmJzZ2Z4MjM0NTQzZ2czcXZhZQ==", "secret_key": "superSecretKey228", "merchant_id": "32"}, "payment_methods": ["card"]}');
+ ('asupayme', 4, '{"credentials": {"api_key": "c2RmZHNhYnZmZGFiYWV0dnJ0c3JhZGZoYnN0cmRmdmJzZ2Z4MjM0NTQzZ2czcXZhZQ==", "secret_key": "superSecretKey228", "merchant_id": "32"}, "payment_methods": ["card"]}'),
+ ('alpex', 5, '{"credentials": {"login": "buyer@dev.alpex.app", "password": "dev", "webhook_url": "https://webhook.site/7cec080f-2cfa-43e8-87f9-92e60651c7e0"}}')
 
 -- auto-generated definition
 create table transaction
