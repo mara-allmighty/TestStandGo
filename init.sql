b08ef9ca5545ff33ba809c8606d69e0fa3a484d8
Марат <marat.tskh@gmail.com>
2025-10-14 15:27:33 +0300

    fix migration NestPay

--- init.sql
+++ init.sql
@@ -33,7 +33,7 @@ VALUES ('paylink', 1, '{"credentials": {"api_key": "m9bb1lelXNK3c198UWb2e41J1EQM
  ('sequoia', 3, '{"credentials": {"secret_key": "8f5DSbXOolXwhLVmtOd", "callback_secret": "fqLFXw9BQLbW7G83Uf"}, "payment_methods": ["p2pcard"]}'),
  ('asupayme', 4, '{"credentials": {"api_key": "c2RmZHNhYnZmZGFiYWV0dnJ0c3JhZGZoYnN0cmRmdmJzZ2Z4MjM0NTQzZ2czcXZhZQ==", "secret_key": "superSecretKey228", "merchant_id": "32"}, "payment_methods": ["card"]}'),
  ('alpex', 5, '{"credentials": {"login": "buyer@dev.alpex.app", "password": "dev", "webhook_url": "https://webhook.site/229ad295-2f54-4a9a-90e8-f6a28ef972c5"}}'),
- ('nestpay', '{"credentials": {"ClientId": "20001223", "Currency": "944", "StoreKey": "stwr3c4v4n9liiryucfhf25520", "Name": "toAdmin", "Password": "TEMP000130432"}}');
+ ('nestpay', 6, '{"credentials": {"ClientId": "20001223", "Currency": "944", "StoreKey": "stwr3c4v4n9liiryucfhf25520", "Name": "toAdmin", "Password": "TEMP000130432"}}');
 
 -- auto-generated definition
 create table transaction
